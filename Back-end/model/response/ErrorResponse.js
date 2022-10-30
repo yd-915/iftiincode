1500431775bd90853b6a63f0bef518cf07a9e9fd
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-10-29 22:01:06 -0400

    Delete Back-end directory

--- Back-end/model/response/ErrorResponse.js
+++
@@ -1,7 +0,0 @@
-module.exports = class ErrorResponse {
-  constructor(statusCode, message, stack) {
-    this.statusCode = statusCode;
-    this.message = message;
-    this.stack = stack;
-  }
-};
--- Back-end/model/response/SuccessResponse.js
+++
@@ -1,7 +0,0 @@
-module.exports = class SuccessResponse {
-  constructor(statusCode, message, data) {
-    this.statusCode = statusCode;
-    this.message = message;
-    this.data = data;
-  }
-};
